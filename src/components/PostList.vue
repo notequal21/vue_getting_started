c8385e75d613c69b88d3a216fed2fa48cbc441c6
notequal21 <kalorigen@gmail.com>
2022-02-13 14:40:16 +0300

    added transiotions

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -3,7 +3,9 @@
     <h3>
       Список пользователей
     </h3>
-    <post-item v-for="post in posts" :key="post.id" :post='post' @remove="$emit('remove', post)" />
+    <transition-group name="post-list">
+      <post-item v-for="post in posts" :key="post.id" :post='post' @remove="$emit('remove', post)" />
+    </transition-group>
   </div>
   <h2 v-else>Постов нет</h2>
 </template>
@@ -22,5 +24,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.2s ease-in-out;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translate(100%, 0);
+}
+.post-list-move {
+  transition: all 0.2s ease-in-out;
+}
 
 </style>
